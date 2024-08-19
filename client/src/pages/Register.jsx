1ee285fc0efa03df46250d1a6cadf65adc112ffe
Mir Mehraj Mushtaq <mehraj95he@gmail.com>
2024-08-19 10:17:46 +0530

    Created navbar and registration form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,49 @@
-const Register =()=>{
-    return <h1>Hello from Register page</h1>
+import "../styles/RegistrationForm.css"
+const Register = () => {
+    return <>
+        <div className="registration-form-container">
+            <h2>Register</h2>
+            <form className="registration-form">
+                <div className="form-group">
+                    <label for="username">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label for="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label for="phone">Phone</label>
+                    <input
+                        type="tel"
+                        id="phone"
+                        name="phone"
+                        required
+                    />
+                </div>
+                <button type="submit" className="submit-button">Register</button>
+            </form>
+        </div>
+    </>
 }
 export default Register;
+
