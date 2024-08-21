64e0ec04aea03678c23a79b82951098f484f4e47
mirmehraj <mehraj95he@gmail.com>
2024-08-21 12:47:18 +0530

    using cors package

--- server/server.js
+++ server/server.js
@@ -3,8 +3,15 @@ import connectDb from "./utils/db.js";
 import router from "./router/auth-router.js"
 import errorMiddleware from "./middlewares/error-middleware.js";
 import contactRouter from "./router/message-router.js"
+import cors from "cors";
 const app = express();
 const PORT = 3000;
+var corsOptions = {
+    origin: 'http://localhost:5173',
+    method: "GET , POST , PUT , PATCH , DELETE , UPDATE , HEAD",
+    Credentials:true,
+  }
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", router);
 app.use("/api/form" , contactRouter);
